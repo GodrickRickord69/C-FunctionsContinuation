e1e96e29f89cee002271111b69f2303d21951901
Nikita <varashilov125@gmail.com>
2023-06-29 15:56:48 +0300

    Task3 Разница макс и мин элементов массива

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,24 +1,39 @@
 ﻿/*Задача 38: Задайте массив целых двухзначных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 [22, 42, 15, 77, 65] => 77 - 15 = 62*/
 
-int[] array = GetArray(5, -100, 100);
+int[] array = GetArray(5, 10, 100);
 Console.WriteLine($"[{String.Join(", ", array)}]");
 
-int Max = array[0];
-int Min = array[0];
 
-for(int i = 0; i < size; i++)
+
+int GetDifferenceMinMax(int []array)
 {
-    if (array[i])
+    int result = 0;
+    int Max = array[0];
+    int Min = array[0];
+    int size = array.Length;
+
+    for (int i = 1; i < size; i++)
+    {
+        if (Max < array[i])
+        {
+            Max = array[i];
+        }
+        if (Min > array[i])
+        {
+            Min = array[i];
+        }
+    }
+    result = Max - Min;
+    return result;
 }
-Console.WriteLine($"Количество четных чисел массива = {EvenNumbers}");
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива = {GetDifferenceMinMax(array)}");
 
 
-
-int [] GetArray(int size, int MinValue, int MaxValue)
+int[] GetArray(int size, int MinValue, int MaxValue)
 {
     int[] result = new int[size];
-    for(int i = 0; i < size - 1; i++)
+    for (int i = 0; i < size; i++)
     {
         result[i] = new Random().Next(MinValue, MaxValue);
     }
